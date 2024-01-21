c4fe198a74e0b625b6d1e9d830e085c71dbf08ba
irshad2k2 <mhdirshad2k2@gmail.com>
2024-01-21 20:35:00 +0530

    updated taskformstate

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -6,6 +6,7 @@ interface TaskFormProps {
 }
 
 interface TaskFormState {
+  id: any;
   title: string;
   description: string;
   dueDate: string;
@@ -13,6 +14,7 @@ interface TaskFormState {
 
 const TaskForm = (props: TaskFormProps) => {
   const [formState, setFormState] = React.useState<TaskFormState>({
+    id: "",
     title: "",
     description: "",
     dueDate: "",
@@ -41,7 +43,7 @@ const TaskForm = (props: TaskFormProps) => {
       return;
     }
     props.addTask(formState);
-    setFormState({ title: "", description: "", dueDate: "" });
+    setFormState({id: "", title: "", description: "", dueDate: "" });
   };
 
   return (
