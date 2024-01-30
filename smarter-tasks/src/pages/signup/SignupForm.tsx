f42c8214704d38c1e55cb754d9cd1af17c7d240b
irshad2k2 <mhdirshad2k2@gmail.com>
2024-01-31 01:50:09 +0530

    removed unused imports

--- smarter-tasks/src/pages/signup/SignupForm.tsx
+++ smarter-tasks/src/pages/signup/SignupForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import { API_ENDPOINT } from "../../config/constants";
 import { useNavigate } from "react-router-dom";
 
