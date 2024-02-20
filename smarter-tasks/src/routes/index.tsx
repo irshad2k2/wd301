ad6db93a4009fc86a065c5446348ef434ec55e22
irshad2k2 <mhdirshad2k2@gmail.com>
2024-02-20 20:10:02 +0530

    Ready for production

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -1,17 +1,19 @@
+import React from "react";
 import AccountLayout from "../layouts/account";
 import ProtectedRoute from "./ProtectedRoutes";
-import Signin from "../pages/signin";
-import Signup from "../pages/signup";
-import Projects from "../pages/projects";
-import Members from "../pages/members";
-import Logout from "../pages/logout";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import Notfound from "../pages/Notfound";
 import ProjectContainer from "../pages/projects/ProjectContainer";
-import ProjectDetails from "../pages/project_details";
-import NewTask from "../pages/tasks/NewTask";
-import TaskDetailsContainer from "../pages/tasks/TaskDetailsContainer";
-
+const Signin = React.lazy(() => import("../pages/signin"));
+const Signup = React.lazy(() => import("../pages/signup"));
+const Projects = React.lazy(() => import("../pages/projects"));
+const Members = React.lazy(() => import("../pages/members"));
+const Logout = React.lazy(() => import("../pages/logout"));
+const ProjectDetails = React.lazy(() => import("../pages/project_details"));
+const NewTask = React.lazy(() => import("../pages/tasks/NewTask"));
+const TaskDetailsContainer = React.lazy(
+  () => import("../pages/tasks/TaskDetailsContainer")
+);
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/account/projects" replace /> },
@@ -46,6 +48,7 @@ const router = createBrowserRouter([
         <AccountLayout />
       </ProtectedRoute>
     ),
+    ErrorBoundary: () => <>Failed to load the page</>,
     children: [
       { index: true, element: <Navigate to="/account/projects" replace /> },
       {
@@ -84,7 +87,6 @@ const router = createBrowserRouter([
       },
     ],
   },
-  
 ]);
 
 export default router;
